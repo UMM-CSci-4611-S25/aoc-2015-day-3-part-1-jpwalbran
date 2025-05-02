d91d43ec4707115211f77f6be3184bd08d57794b
John Walbran <32685970+jpwalbran@users.noreply.github.com>
2025-05-01 21:26:47 -0500

    Update for later submission
    I made a few changes, based on previous feedback.
    1. I introduced the idea of a `FileIOError`, which covers both an incorrect file path, as well as illegal characters.
    2. I refactored the code for handling position tracking into its own function `process_string()` for testing and neatness.
    3. I added testing to ensure that both forms of the input handling works, as well as testing the overall processing based on the given inputs.

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,117 @@
 use std::collections::HashSet;
 
-fn main() {
-    main_part_1();
-    main_part_2();
+#[derive(Debug)]
+enum FileIOError{
+    InvalidCharError(String),
+    FileReadError(String)
 }
 
-fn main_part_1() {
-    let input_file_name = "input.txt";
-    let contents = std::fs::read_to_string(input_file_name).expect("Failed to read the input file");
-    let mut visited: HashSet<(i32, i32)> = HashSet::new();
-    let mut pos: (i32, i32) = (0, 0);
-    visited.insert(pos);
-
-    for c in contents.chars() {
-        if c == '^' {
-            pos.1 += 1; 
-        }
-        if c == 'v' {
-            pos.1 -= 1;
-        }
-        if c == '<' {
-            pos.0 -= 1;
-        }
-        if c == '>' {
-            pos.0 += 1;
-        }
-        visited.insert(pos);    
-    }
-    println!("Part 1: {}", visited.len());
-
+fn main() -> Result<(), FileIOError> {
+    let p1 = main_part_1()?;
+    let p2 = main_part_2()?;
+    println!("Part 1: {p1}");
+    println!("Part 2: {p2}");
+    Ok(())
 }
 
-fn main_part_2() {
-    let input_file_name = "input.txt";
-    let contents = std::fs::read_to_string(input_file_name).expect("Failed to read the input file");
+fn process_string(input: String, robo_santa: bool) -> Result<usize, FileIOError> {
     let mut visited: HashSet<(i32, i32)> = HashSet::new();
     let mut santa_pos: (i32, i32) = (0, 0);
-    let mut robo_santa_pos: (i32, i32) = (0,0);
+    let mut robo_santa_pos = (0,0);
+    if robo_santa {
+        robo_santa_pos = (0,0);
+    }
     visited.insert(santa_pos);
 
-    for (i,c) in contents.chars().enumerate() {
-        let pos = if i & 1 == 0 {&mut robo_santa_pos} else {&mut santa_pos};
+    for (i,c) in input.chars().enumerate() {
+        let pos: &mut (i32, i32);
+        if robo_santa {
+            pos = if i & 1 == 0 {&mut robo_santa_pos} else {&mut santa_pos};
+        } else {
+            pos = &mut santa_pos;
+        }
         if c == '^' {
             pos.1 += 1; 
         }
-        if c == 'v' {
+        else if c == 'v' {
             pos.1 -= 1;
         }
-        if c == '<' {
+        else if c == '<' {
             pos.0 -= 1;
         }
-        if c == '>' {
+        else if c == '>' {
             pos.0 += 1;
+        }
+        //Ignore the end of file newline character.
+        else if c == '\n' {
+            continue;
+        } 
+        else {
+            return Err(FileIOError::InvalidCharError(
+                std::format!("Invalid Character {c}, expected one of: '^v<>\n'.").to_string()
+            ));
         }
         visited.insert(*pos);    
     }
-    println!("Part 2: {}", visited.len());
+    Ok(visited.len())
+}
 
+fn main_part_1() -> Result<usize, FileIOError> {
+    let input_file_name = "input.txt";
+    let contents = match std::fs::read_to_string(input_file_name) {
+        Ok(str) => str,
+        Err(_) => {
+            return Err(
+                FileIOError::FileReadError(
+                    std::format!("Could not read file {input_file_name}").to_string()
+                ));
+        }
+    };
+    let visited_count = process_string(contents, false)?; 
+    Ok(visited_count)
+}
+
+fn main_part_2() -> Result<usize, FileIOError> {
+    let input_file_name = "input.txt";
+    let contents = match std::fs::read_to_string(input_file_name) {
+        Ok(str) => str,
+        Err(_) => {
+            return Err(
+                FileIOError::FileReadError(
+                    std::format!("Could not read file {input_file_name}").to_string()
+                ));
+        }
+    };
+    let visited_count = process_string(contents, true)?;
+    Ok(visited_count)
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
+
+    #[test]
+    fn test_single_moves() -> Result<(), FileIOError> {
+        let test_pattern = "^>v<<".to_string();
+        let expected_count_single_santa = 5;
+        let expected_count_robo_santa = 4;
+        assert_eq!(process_string(test_pattern.clone(), false)?, expected_count_single_santa);
+        assert_eq!(process_string(test_pattern.clone(), true)?, expected_count_robo_santa);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part_1() -> Result<(), FileIOError> {
+        assert_eq!(main_part_1()?, 2565);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part_2() -> Result<(), FileIOError> {
+        assert_eq!(main_part_2()?, 2639);
+        Ok(())
+    }
 
     // #[test]
     // fn test_visited_houses_new() {
